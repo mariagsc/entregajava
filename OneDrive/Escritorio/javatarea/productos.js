f2eb7824a5214b49f42690728ea8aea3b77112e4
mariagsc <sandovalcontrerasmariagabriela@gmail.com>
2025-07-27 18:02:29 -0400

    Primer commit

---
+++ OneDrive/Escritorio/javatarea/productos.js
@@ -0,0 +1,13 @@
+ const productos = [
+  { nombre: "PUMA CLASICA",
+     precio: 44000, 
+     img: "../javatarea/img/Puma37.webp" },
+
+  { nombre: "Adidas Ultra",
+     precio: 44000, 
+     img: "../javatarea/img/adidas-ultra.webp" },
+     
+  { nombre: "Adidas Nova", 
+    precio: 50000,
+     img: "../javatarea/img/zapatilla-adidas-nova.jpg" }
+];
