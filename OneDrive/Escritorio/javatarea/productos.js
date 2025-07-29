a007ed5310af6abcd2a543c34ac62f060c0bfb11
mariagsc <sandovalcontrerasmariagabriela@gmail.com>
2025-07-28 23:16:10 -0400

    Corrijo rutas de imágenes y error de sintaxis

--- OneDrive/Escritorio/javatarea/productos.js
+++ OneDrive/Escritorio/javatarea/productos.js
@@ -1,13 +1,17 @@
-const productos = [
-  { nombre: "PUMA CLASICA",
-     precio: 44000, 
-     img: "../javatarea/img/Puma37.webp" },
-
-  { nombre: "Adidas Ultra",
-     precio: 44000, 
-     img: "../javatarea/img/adidas-ultra.webp" },
-     
-  { nombre: "Adidas Nova", 
+ const productos = [
+  {
+    nombre: "PUMA CLÁSICA",
+    precio: 44000,
+    img: "img/puma37.webp"
+  },
+  {
+    nombre: "Adidas Ultra",
+    precio: 44000,
+    img: "img/adidas-ultra.webp"
+  },
+  {
+    nombre: "Adidas Nova",
     precio: 50000,
-     img: "../javatarea/img/zapatilla-adidas-nova.jpg" }
+    img: "img/zapatilla-adidas-nova.jpg"
+  }
 ];
